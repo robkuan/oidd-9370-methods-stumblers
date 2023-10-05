ba93091cc06529686c69f7c49cf501fd474e798c
robkuan <89936263+robkuan@users.noreply.github.com>
2023-10-05 08:50:30 -0400

    hw 3

---
+++ hws/hw-3/hw-3.R
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# CALCULATING THE NULL - HW 3
+
+# Import libraries
+library("tidyverse")
+
+#-------------------------------------------------------------------------------
+# QUESTION 1
+
+# Import data from control condition and clean data
+df_raw <- read_csv("data/probowl nfl.csv") # read in data
+
+df_
+
